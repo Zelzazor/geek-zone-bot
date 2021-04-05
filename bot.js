0ece443e956f7e4c8d79e9faadd0a569db8334e5
Felipe Ramírez <zelzazor.ramirez62@gmail.com>
2021-04-05 19:16:20 -0400

    Creados comandos

---
+++ bot.js
@@ -0,0 +1,19 @@
+import TelegramBot from 'node-telegram-bot-api';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const token = process.env.TOKEN;
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/^\/echo (.+)/, (msg, match) => {
+    
+    const chatId = msg.chat.id;
+    const resp = match[1]; 
+
+    // send back the matched "whatever" to the chat
+    bot.sendMessage(chatId, resp);
+       
+});
+
