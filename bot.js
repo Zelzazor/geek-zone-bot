28374a8e41f960f57696919d9bae326cd7432bbf
Felipe Ramírez <zelzazor.ramirez62@gmail.com>
2021-04-06 22:09:41 -0400

    Añadido comando rae

--- bot.js
+++ bot.js
@@ -1,14 +1,19 @@
 import TelegramBot from 'node-telegram-bot-api';
 import dotenv from 'dotenv';
 import responses from './commands/responses.js';
+import Rae from 'rae';
 
 if (process.env.NODE_ENV !== 'production') { 
     dotenv.config() 
 }
 
-const {donate, options} = responses;
+const {donate, options, dictionary} = responses;
 
 const token = process.env.TOKEN;
+const oxford_id = process.env.APP_ID_OXFORD;
+const oxford_key = process.env.APP_KEY_OXFORD;
+const raeClient = Rae.create();
+
 
 
 const bot = new TelegramBot(token, {polling: true});
@@ -27,4 +32,25 @@ bot.onText(/^\/donate/, (msg) => {
     const chatId = msg.chat.id;
     bot.sendMessage(chatId, donate, options(msg));
 });
+
+bot.onText(/^\/diccionario (.+)/,  (msg, match) => {
+
+    (async ()=>{
+            const word = match[1];
+            const init = {
+                method: 'GET',
+                headers: {
+                    'app_id': oxford_id,
+                    'app_key': oxford_key
+                }
+            }
+            const res = await fetch(`https://od-api.oxforddictionaries.com/api/v2/entries/es/${word}`, init);
+            const data = await res.json();
+            const senses = data.results[0].lexicalEntries[0].entries[0].senses;
+            const definitions = senses.map(sense => sense.definitions[0]);
+            //console.log(definitions);
+            bot.sendMessage(msg.chat.id, dictionary(word, definitions), options(msg));
+    })();
+
+})
 
--- commands/responses.js
+++ commands/responses.js
@@ -1,5 +1,12 @@
 export default {
     donate: "Dona a este enlace carajo:\nhttps://www.buymeacoffee.com/zelzazor",
+    dictionary: function(word, definitions){
+        let formatStr = word+":\n";
+        for(const definition of definitions)  {
+            formatStr+="1. "+definition+"\n";
+        }
+        return formatStr;
+    },
     options: function(message){return {
 		parse_mode: 'Markdown',
 		reply_to_message_id: message.message_id,
